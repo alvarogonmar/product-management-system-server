59cd242f4981d7c500c8ca5cc968d2a9c3420ac7
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-12 20:52:15 -0600

    fix: wrap product creation logic in try-catch for error handling

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,9 +3,10 @@ import Product from "../models/Product.model";
 
 export const createProduct = async (req: Request, res: Response) => {
     // Logic to create a product
-
-
-
-    const product = await Product.create(req.body);
-    res.json({data: product});
+    try {       
+        const product = await Product.create(req.body);
+        res.json({data: product});
+    } catch (error) {
+        console.log(error);
+    }
 }
