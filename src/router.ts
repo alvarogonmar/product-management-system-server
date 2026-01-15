d4b92020d8ab250037e06771c69887069a1a9a25
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-14 20:53:37 -0600

    fix: update product route to use updateAvailability handler in PATCH request

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { body, param } from "express-validator";
-import { createProduct, getProductById, getProducts, updateProduct } from "./handlers/product";
+import { createProduct, getProductById, getProducts, updateAvailability, updateProduct } from "./handlers/product";
 import { handleInputErrors } from "./middleware";
 
 const router = Router();
@@ -36,9 +36,7 @@ router.put("/:id",
     updateProduct
 )
 
-router.patch("/", (req, res) => { 
-    res.json({"Hello, World!": "PATCH"});
-})
+router.patch("/", updateAvailability);
 
 router.delete("/", (req, res) => {
     res.json({"Hello, World!": "DELETE"});
