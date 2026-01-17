e18c1aadca6cd9cb81bc45fc31fa30e014252937
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-16 21:02:37 -0600

    feat: enhance clearDB function with success message logging

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,6 +4,8 @@ import db from '../config/db';
 const clearDB = async () => {
     try {
         await db.sync({force: true});
+        console.log("Database cleared successfully.");
+        exit(0);
     } catch (error) {
         console.error("Error clearing the database:", error);
         exit(1);
