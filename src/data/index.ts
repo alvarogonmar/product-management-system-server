50585e8272544a5fa574023e5f051e41130876dd
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-16 21:02:21 -0600

    feat: implement clearDB function to force sync database and handle errors

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,2 +1,10 @@
 import { exit } from 'node:process';
 import db from '../config/db';
+
+const clearDB = async () => {
+    try {
+        await db.sync({force: true});
+    } catch (error) {
+        console.error("Error clearing the database:", error);
+        exit(1);
+    }}
