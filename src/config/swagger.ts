7ea28347c9cd84513c7a5593bf8ed0d17ae6c9d5
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-20 21:39:51 -0600

    feat: add tags to swagger definition for product management endpoints

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -3,6 +3,18 @@ import swaggerJSDoc from "swagger-jsdoc";
 const options: swaggerJSDoc.Options = {
     swaggerDefinition: {
         openapi: "3.0.0",
+        tags: [
+            {
+                name: "Products",
+                description: "API endpoints for managing products"
+            }
+        ],
         info: {
-            
+            title: "Products API REST API Node.js / Express / TypeScript",
+            version: "1.0.0",
+            description: "API RESTful for managing products using Node.js, Express, and TypeScript."
+        },
+    },
+        apis: []
+
 };
