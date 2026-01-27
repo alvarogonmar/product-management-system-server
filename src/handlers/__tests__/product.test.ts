a52398e0af9d98db91e73cad4acfbcdb898481b2
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-26 22:03:52 -0600

    fix: update assertion in PATCH test for product availability to ensure no 400 error

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -205,7 +205,7 @@ describe("PATCH /api/productos/:id", () => {
         expect(response.body.data.availability).toBe(false);
 
         expect(response.status).not.toBe(404);
-        expect(response.status).not.toBe(400); // Ensure it's not a 400 error
+        expect(response.status).not.toBe(400);
         expect(response.body).not.toHaveProperty("error");  // Ensure there's no error property
     });
 });
