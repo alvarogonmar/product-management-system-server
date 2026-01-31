7f419f1bfaeccb9b30edbe50895723de3281a5a6
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-30 21:33:11 -0600

    fix: update assertions in PATCH test for product availability

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -204,7 +204,7 @@ describe("PATCH /api/productos/:id", () => {
         expect(response.body).toHaveProperty("data");
         expect(response.body.data.availability).toBe(false);
 
-        expect(response.status).not.toBe(404); // Ensure status is not 404
+        expect(response.status).not.toBe(404);
         expect(response.status).not.toBe(400); // Ensure status is not 400
         expect(response.body).not.toHaveProperty("error");
     });
