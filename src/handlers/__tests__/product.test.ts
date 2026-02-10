addd782cd546cae99c02bcab96f78960aaed7b91
alvarogonmar <alvarogonmar0@gmail.com>
2026-02-09 21:45:59 -0600

    fix: remove redundant comments in PATCH request test for non-existent product

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -184,7 +184,7 @@ describe("PUT /api/productos/:id", () => {
 });
 
 describe("PATCH /api/productos/:id", () => {
-    it("should return a 404 response for a non existent product", async () => { // Make the PATCH request to update the product availability for a non-existent product
+    it("should return a 404 response for a non existent product", async () => {
         const productID = 2000 // Define a non-existent product ID
         const response = await request(server) // Make the PATCH request to update the product availability for a non-existent product
             .patch(`/api/productos/${productID}`);// Make the PATCH request to update the product availability for a non-existent product
