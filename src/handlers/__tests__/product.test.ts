b603310a04aa28ef3b6527b88d48ec60dbb0d2d0
alvarogonmar <alvarogonmar0@gmail.com>
2026-02-15 21:00:55 -0600

    fix: update comment for PATCH request to check 404 response for non-existent product

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -187,7 +187,7 @@ describe("PATCH /api/productos/:id", () => {
     it("should return a 404 response for a non existent product", async () => {
         const productID = 2000
         const response = await request(server)
-            .patch(`/api/productos/${productID}`); // No body is needed for this test since we're only checking the response for a non-existent product
+            .patch(`/api/productos/${productID}`);
 
         expect(response.status).toBe(404); // Assert that the response status is 404 Not Found
         expect(response.body.error).toBe("Product not found"); // Assert that the response body contains the expected error message
