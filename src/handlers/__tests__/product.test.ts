f73dca65e2cc17b51504f91f37dae877883e1a78
alvarogonmar <alvarogonmar0@gmail.com>
2026-02-16 18:55:27 -0600

    fix: clarify comment for 404 response in PATCH request for non-existent product

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -184,7 +184,7 @@ describe("PUT /api/productos/:id", () => {
 });
 
 describe("PATCH /api/productos/:id", () => { // PATCH para actualizar solo la disponibilidad
-    it("should return a 404 response for a non existent product", async () => {
+    it("should return a 404 response for a non existent product", async () => { // PATCH para actualizar solo la disponibilidad
         const productID = 2000
         const response = await request(server)
             .patch(`/api/productos/${productID}`);
