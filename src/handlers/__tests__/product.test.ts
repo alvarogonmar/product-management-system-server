c9906782c3e2b15ba958d8f9da442cff0fe348c0
alvarogonmar <alvarogonmar0@gmail.com>
2026-02-17 20:18:27 -0600

    fix: remove unnecessary comment in PATCH request test for product availability

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -194,7 +194,7 @@ describe("PATCH /api/productos/:id", () => {
         expect(response.body).not.toHaveProperty("data");
     });
 
-    it("should update the product availability", async () => { // Test case to check if the PATCH endpoint correctly updates the product's availability
+    it("should update the product availability", async () => {
         const response = await request(server) // Make the PATCH request to toggle availability for product with ID 1
             .patch(`/api/productos/1`);
 
