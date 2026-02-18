66a53fe844a7548130902848c48027664ae7064c
alvarogonmar <alvarogonmar0@gmail.com>
2026-02-17 20:18:09 -0600

    fix: remove unnecessary comment for PATCH request to return 404 for non-existent product

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -190,7 +190,7 @@ describe("PATCH /api/productos/:id", () => {
             .patch(`/api/productos/${productID}`);
 
         expect(response.status).toBe(404);
-        expect(response.body.error).toBe("Product not found"); // Expect the error message to indicate the product was not found
+        expect(response.body.error).toBe("Product not found");
         expect(response.body).not.toHaveProperty("data"); // Ensure that the response does not contain a data property
     });
 
