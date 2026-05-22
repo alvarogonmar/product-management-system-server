3e216b599d077134b752242f9c1cbb643af96405
alvarogonmar <alvarogonmar0@gmail.com>
2026-05-21 19:11:16 -0600

    test: ensure successful response status for product availability update

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -205,7 +205,7 @@ describe("PATCH /api/productos/:id", () => {
         expect(response.body.data.availability).toBe(false);
 
         expect(response.status).not.toBe(404); // Ensures the product was found
-        expect(response.status).not.toBe(400); // Ensures the response is successful
+        expect(response.status).not.toBe(400);
         expect(response.body).not.toHaveProperty("error");
     });
 });
