446e700b5e27fadb2f7aaea81c789f848a543b0b
alvarogonmar <alvarogonmar0@gmail.com>
2026-06-13 10:54:32 -0600

    test: remove redundant comment in DELETE request for non-existent product

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -226,7 +226,7 @@ describe("DELETE /api/productos/:id", () => {
         const response = await request(server)
             .delete(`/api/productos/${productID}`);
             
-        expect(response.status).toBe(404); // Verifies that the response status is 404 Not Found
+        expect(response.status).toBe(404);
         expect(response.body.error).toBe("Product not found");
 
         expect(response.status).not.toBe(200);
