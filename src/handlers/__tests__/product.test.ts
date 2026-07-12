4aec1ab4c986a22e6d3ac1793a538067b6d9db5e
alvarogonmar <alvarogonmar0@gmail.com>
2026-07-12 12:00:00 -0600

    updated

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -187,7 +187,7 @@ describe("PUT /api/productos/:id", () => {
 
 describe("PATCH /api/productos/:id", () => {
     it("should return a 404 response for a non existent product", async () => { // Test case for non-existent product
-        const productID = 2000 // Assuming this ID does not exist in the database
+        const productID = 2000
         const response = await request(server)
             .patch(`/api/productos/${productID}`); 
 
