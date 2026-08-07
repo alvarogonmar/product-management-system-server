d0c251386311dbed77a37f92ec05362b8f6293e8
alvarogonmar <alvarogonmar0@gmail.com>
2026-08-07 12:00:00 -0600

    updated

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -192,7 +192,7 @@ describe("PATCH /api/productos/:id", () => {
             .patch(`/api/productos/${productID}`); // Send the request without a body since PATCH is used to update availability
 
         expect(response.status).toBe(404); // Expect the response status to be 404 (Not Found)
-        expect(response.body.error).toBe("Product not found");
+        expect(response.body.error).toBe("Product not found"); // Expect the response body to contain an error message indicating the product was not found
         expect(response.body).not.toHaveProperty("data");
     });
 
